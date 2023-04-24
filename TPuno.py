493255ce54451a884318a02c5cea3f6421b06086
Your Name <you@example.com>
2023-04-24 18:26:29 -0300

    hice algunos cambios para cumplir el objetivo del tp y guardo antes de probar unos cambios importantes

--- TPuno.py
+++ TPuno.py
@@ -24,19 +24,21 @@ while pasw != "12345" and intent < 3:
 
 class local:
     nombre = ""
-    ubicacion = ""
-    rubro = ""
+    calle = ""
+    numero = 0
+    color = ""
 
-    def __init__(self, nombre, ubicacion, rubro):
+    def __init__(self, nombre, calle, numero, color):
         self.nombre = nombre
-        self.ubicacion = ubicacion
-        self.rubro = rubro
+        self.calle = calle
+        self.numero = numero
+        self.color = color
 
 locales = []
 
 
 while user == "admin@shopping.com" and pasw == "12345" and salida == 0:
-    print("Menú de opciones:")
+    print("Bienvenido de vuelta administrador\n\n Menú de opciones:")
     print("1. Crear local")
     print("2. Colnsultar locales")
     print("0. Salir del sistema")
@@ -46,10 +48,11 @@ while user == "admin@shopping.com" and pasw == "12345" and salida == 0:
     if option == 1:
         if len(locales) < 5:
             nombre = input("Ingrese el nombre del nuevo local: ")
-            ubicacion = input("Ingresa la ubicación del local: ")
-            rubro = input("A que rubro pertenece el local? (indumentaria, perfumería o gastronomía)")
+            calle = input("Ingrese la calle del local: ")
+            numero = int(input("Ingrese el número de la calle: "))
+            color = input("Ingrese el color del local: ")
 
-            local_nuevo = local(nombre, ubicacion, rubro)
+            local_nuevo = local(nombre, calle, numero, color)
             locales.append(local_nuevo)
             print("el local se ha creado exitosamente")
         else:
@@ -68,8 +71,7 @@ while user == "admin@shopping.com" and pasw == "12345" and salida == 0:
             print("El número ingresado no es valido")
         else:
             local_seleccionado = locales[num_local - 1]
-            print("Ha seleccionado el local: {}\nUbicación: {}\nRubro: {}".format(local_seleccionado.nombre, local_seleccionado.ubicacion, local_seleccionado.rubro))
-            vuelta = input("Presiona Enter para continuar")
+            print("Ha seleccionado el local: {}\nCalle: {}\nNúmero: {}\nColor: {}".format(local_seleccionado.nombre, local_seleccionado.calle, local_seleccionado.numero, local_seleccionado.color))
     
     elif option == 0:
         print("Gracias por utilizar nuestro sistema")
